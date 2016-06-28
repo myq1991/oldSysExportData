d29832702d3b9faeb22895c4b36f4a7634697529
myq1991 <myq1991@gmail.com>
2016-06-29 05:36:51 +0800

    init

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=swim_sysmgnt',
-    'username' => 'dev',
-    'password' => '123456',
+    'username' => 'root',
+    'password' => 'mayeqin911104',
     'charset' => 'utf8',
 ];
